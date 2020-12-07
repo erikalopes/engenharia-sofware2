45eda8055f78f9f6fccd8d632ff433d2585eb359
Erika Lopes <201810099@acad.unijuazeiro.edu.br>
2020-12-06 21:41:06 -0300

    corrigido texto do test

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders unijuazeiro link', () => {
   render(<App />);
   const linkElement = screen.getByText(/Conheça a Unijuazeiro/i);
   expect(linkElement).toBeInTheDocument();
